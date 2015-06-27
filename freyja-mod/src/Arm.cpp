0369eaa9d79248b15824e29a14bc705e19f5504c
Driver Station <jngo@palyrobotics.com>
2015-06-27 16:09:26 -0700

    Fill out Arm

--- freyja-mod/src/Arm.cpp
+++ freyja-mod/src/Arm.cpp
@@ -1,27 +1,85 @@
 #include <Arm.h>
 
-Arm::Arm() {
+Arm::Arm() : comp(), sol((uint32_t) 0), timer() {
 
 }
 
 void Arm::init() {
-
+	compState = ON;
+	setState(IDLE_OPEN);
 }
 
 void Arm::update(){
+	switch(state) {
+		case IDLE_OPEN:
+		case IDLE_CLOSED:
+			sol.Set(DoubleSolenoid::Value::kOff);
+			timer.Stop();
+			timer.Reset();
+			break;
+		case OPENING:
+			sol.Set(DoubleSolenoid::Value::kReverse);
+			if(timer.Get() >= ARM_EXTEND_TIME) {
+				setState(IDLE_OPEN);
+			}
+			break;
+		case CLOSING:
+			sol.Set(DoubleSolenoid::Value::kForward);
+			if(timer.Get() >= ARM_EXTEND_TIME) {
+				setState(IDLE_CLOSED);
+			}
+			break;
+	}
 
+	switch(compState) {
+		case ON:
+			comp.Start();
+			break;
+		case OFF:
+			comp.Stop();
+			break;
+	}
 }
 
 void Arm::disable(){
-
+	setState(STOPPED);
 }
 
 void Arm::idle() {
-
+	//do nothing
 }
 
 bool Arm::isIdle() {
+	return (state == IDLE_OPEN || state == IDLE_CLOSED);
+}
 
+void Arm::setState(State newState) {
+	if(state!=STOPPED) {
+		state = newState;
+	}
+}
+
+void Arm::toggle() {
+	switch(state) {
+		case IDLE_OPEN:
+		case OPENING:
+			open();
+			break;
+		case IDLE_CLOSED:
+		case CLOSING:
+
+			break;
+	}
+}
+
+void Arm::open() {
+	setState(OPENING);
+	timer.Start();
+}
+
+void Arm::close() {
+	setState(CLOSING);
+	timer.Start();
 }
 
 Arm::~Arm() {
--- freyja-mod/src/Arm.h
+++ freyja-mod/src/Arm.h
@@ -5,6 +5,8 @@
 #include "Subsys.h"
 #include <Ports.h>
 
+#define ARM_EXTEND_TIME 0.4
+
 /**
  * Represents Freyja's pneumatically controlled arms
  */
@@ -19,8 +21,30 @@ public:
 	void disable();
 	void idle();
 	bool isIdle();
+	void toggle();
 
 private:
+	Compressor comp;
+	DoubleSolenoid sol;
+	Timer timer;
+
+	enum State {
+		IDLE_OPEN,
+		IDLE_CLOSED,
+		OPENING,
+		CLOSING,
+		STOPPED
+	};
+	enum CompressorState {
+		ON, OFF
+	};
+
+	State state;
+	CompressorState compState;
+
+	void setState(State);
+	void open();
+	void close();
 
 };
 
