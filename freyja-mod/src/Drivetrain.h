19da5b90a3a412f5a6af537784fa79432c901774
Eric Liu <ericxliu@gmail.com>
2015-06-28 16:34:16 -0700

    added comments

--- freyja-mod/src/Drivetrain.h
+++ freyja-mod/src/Drivetrain.h
@@ -11,23 +11,29 @@
 #include <Ports.h>
 
 // MAX_FORWARD_SPEED + MAX_TURN_SPEED should not exceed 1.0
+//Constants for regulating speed
 #define MAX_FORWARD_SPEED 0.0
 #define MAX_TURN_SPEED 0.0
 #define SPEED_CONSTANT 0.0
 #define TURN_CONSTANT 0.0
 
+//Left PID constants
 #define LEFT_DRIVE_PROPORTIONAL 0.0
 #define LEFT_DRIVE_INTEGRAL 0.0
 #define LEFT_DRIVE_DERIVATIVE 0.0
 
+
+//Right PID constants
 #define RIGHT_DRIVE_PROPORTIONAL 0.0
 #define RIGHT_DRIVE_INTEGRAL 0.0
 #define RIGHT_DRIVE_DERIVATIVE 0.0
 
+ //Gyro PID constants
 #define GYRO_PROPORTIONAL 0.0
 #define GYRO_INTEGRAL 0.0
 #define GYRO_DERIVATIVE 0.0
 
+//Encoder constants
 #define RIGHT_DPP 0.0
 #define LEFT_DPP 0.0
 #define ENCODER_MAX_PERIOD 0
@@ -35,12 +41,15 @@
 #define ENCODER_DRIVE_OUTPUT_RANGE 0
 #define ENCODER_GYRO_OUTPUT_RANGE 0
 
+//Acceptable error constants
 #define ACCEPTABLE_DRIVE_ERROR 1
 #define ACCEPTABLE_ROTATE_ERROR 1
 #define ACCEPTABLE_BRAKE_ERROR 0.01
 
 /**
- * Represents Freyja's West-Coast Drivetrain
+ * The drivetrain is a subsystem of the robot that moves it around. It has 2 encoders; a left one and a right one.
+ * The drivetrain also has a gyro and two PID controllers
+ *
  */
 class Drivetrain : public Subsys {
 public:
