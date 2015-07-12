0c58662d968eaaad6a0effdc089a25d18aab5775
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-07-12 15:48:54 -0700

    Added drivetrain and braking functionality to TeleopCtrlr

--- freyja-mod/src/TeleopController.cpp
+++ freyja-mod/src/TeleopController.cpp
@@ -20,8 +20,24 @@ void TeleopController::init() {
 }
 
 void TeleopController::update(Robot *const robot) {
-	drivetrain->drive(turnJoystick.GetX(), driveJoystick.GetY());
-	lifter->setVelocity(operatorJoystick.GetY());
+	// Drivetrain controls, will drive only if brake not being called
+	// Brake will only be called if it is the first call
+	bool previouslyBraking = false;
+	if(driveJoystick.GetRawButton(1)) {
+		if(!previouslyBraking) {
+			drivetrain->brake();
+		}
+		previouslyBraking = true;
+	}
+	else {
+		previouslyBraking = false;
+		drivetrain->drive(turnJoystick.GetX(), driveJoystick.GetY());
+	}
+
+	arm->update();
+	drivetrain->update();
+	lifter->update();
+	ramp->update();
 }
 
 void TeleopController::disable() {
